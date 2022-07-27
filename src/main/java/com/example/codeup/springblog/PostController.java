75a5ca98fb1f04421b8e150a4e6b46c8994b68fc
Lineisha <lineishagibson@gmail.com>
2022-07-27 16:38:57 -0500

    posts

--- src/main/java/com/example/codeup/springblog/PostController.java
+++ src/main/java/com/example/codeup/springblog/PostController.java
@@ -1,9 +1,12 @@
 package com.example.codeup.springblog;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class PostController {
 @GetMapping("/posts")
     @ResponseBody
@@ -14,6 +17,16 @@ public class PostController {
     @ResponseBody
     public String getPost(@PathVariable long id){
     return "Viewing post: " + id;
+}
+@GetMapping("/posts/create")
+    @ResponseBody
+    public String getCreatePostForm(){
+    return "Form to create post";
+}
+@PostMapping("/posts/create")
+    @ResponseBody
+    public String savePost(){
+    return "Saving post";
 }
 }
 //GET	/posts	posts index page
