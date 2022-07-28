3a78f30fd16bb49fdd46aa7bb3ff28ab6e962291
Lineisha <lineishagibson@gmail.com>
2022-07-27 21:21:08 -0500

    partial

--- src/main/java/com/example/codeup/springblog/PostController.java
+++ src/main/java/com/example/codeup/springblog/PostController.java
@@ -2,10 +2,7 @@ package com.example.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,18 +18,22 @@ public class PostController {
     return "posts/index";
 }
 @GetMapping("/posts/{id}")
-    @ResponseBody
+//    @ResponseBody
     public String getPost(@PathVariable long id){
     return "Viewing post: " + id;
 }
+
 @GetMapping("/posts/create")
-    @ResponseBody
-    public String getCreatePostForm(){
-    return "Form to create post";
+//    @ResponseBody
+    public String getCreatePostForm(@RequestParam String query, Model model) {
+    model.addAttribute("create", query);
+    return "create";
 }
+
 @PostMapping("/posts/create")
-    @ResponseBody
-    public String savePost(){
-    return "Saving post";
+//    @ResponseBody
+    public String savePost(@RequestParam String query, Model model) {
+    model.addAttribute("create", query);
+    return "show";
 }
 }
