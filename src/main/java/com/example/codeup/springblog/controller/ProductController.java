0b832eff0e41742d1c78166dfe50be6b60621820
Lineisha <lineishagibson@gmail.com>
2022-08-02 10:03:00 -0500

    products

--- src/main/java/com/example/codeup/springblog/controller/ProductController.java
+++ src/main/java/com/example/codeup/springblog/controller/ProductController.java
@@ -1,23 +1,30 @@
 package com.example.codeup.springblog.controller;
 
+import com.example.codeup.springblog.model.Product;
+import com.example.codeup.springblog.repositories.PostRepository;
 import com.example.codeup.springblog.repositories.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-public class ProductController {
-    private final ProductRepository productDao;
+import java.util.List;
 
-public ProductController(ProductRepository productDao) {
-            this.productDao = productDao;
-        }
-
-        @GetMapping("/products")
-        public String index(Model model){
-            model.addAttribute("product",productDao.findAll());
-            return "products/index";
-        }
+//@Controller
+//public class ProductController {
+//    private final ProductRepository productDao;
+//private final PostRepository postDao;
+//
+// public ProductController(ProductRepository productDao) {
+//            this.productDao = productDao;
+// this.postDao = postDao;
+//        }
+//
+//        @GetMapping("/products")
+//        public String showAllProducts(Model model){
+//            List<Product> productList = productDao.findAll();
+//            model.addAttribute("product",productList);
+//            return "products/index";
+//        }
 
 
 
@@ -31,4 +38,77 @@ public ProductController(ProductRepository productDao) {
 //       model.addAttribute("product",productList);
 //return "products/index";
 //    }
+//}
+@Controller
+public class ProductController {
+
+    // dependency injection
+    private ProductRepository productDao;
+    private PostRepository postDao;
+
+    public ProductController(ProductRepository productDao, PostRepository postDao) {
+        this.productDao = productDao;
+        this.postDao = postDao;
+    }
+
+    // get all records with JPA
+    @GetMapping("/products")
+    public String showAllProducts(Model vModel) {
+        List<Product> productList = productDao.findAll();
+        // pass products to view
+        vModel.addAttribute("products", productList);
+        return "products/index";
+    }
+
+    @GetMapping("/products/create")
+    public String createProducts() {
+        return "products/create";
+    }
+
+    @PostMapping("/products/create")
+    public String submitProduct(@RequestParam String name, @RequestParam int priceInCents) {
+        Product product = new Product(name, priceInCents);
+        productDao.save(product);
+        return "redirect:/products";
+    }
+
+    // delete a record with JPA
+    @GetMapping("/products/delete/{id}")
+    public String deleteProduct(@PathVariable long id) {
+        productDao.deleteById(id);
+        return "redirect:/products";
+    }
+
+    // getting a specific record with JPA
+    @GetMapping("/products/test/{id}")
+    public String getProduct(@PathVariable long id) {
+        Product p = productDao.findById(id).get();
+        System.out.println(p);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/derived/1")
+    @ResponseBody
+    public List<Product> getProductsAfterId() {
+        return productDao.getProductsByIdAfter(6L);
+    }
+
+    @GetMapping("/derived/2")
+    @ResponseBody
+    public List<Product> getBirdProducts() {
+        return productDao.getProductByNameEndsWithIgnoreCase("bird");
+    }
+
+    @GetMapping("/derived/3")
+    @ResponseBody
+    public Product getProductByPrice() {
+        return productDao.getFirstByPriceInCents(55);
+    }
+
+    @GetMapping("/test/jpql")
+    @ResponseBody
+    public List<String> getProductFirstName() {
+        return productDao.getProductNamesNative();
+    }
+
 }
--- src/main/java/com/example/codeup/springblog/model/Product.java
+++ src/main/java/com/example/codeup/springblog/model/Product.java
@@ -3,16 +3,17 @@ package com.example.codeup.springblog.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "product")
+@Table(name = "products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "INT")
     private long id;
+
+@Column(length = 100, nullable = false)
     private String name;
 
-
-    private int priceInCents;
+ private int priceInCents;
 
     public Product(String name, int priceInCents) {
         this.name = name;
@@ -38,4 +39,12 @@ public class Product {
     public void setPriceInCents(int priceInCents) {
         this.priceInCents = priceInCents;
     }
+@Override
+public String toString() {
+        return "Product{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", priceInCents=" + priceInCents +
+        '}';
+        }
 }
